b04d8eb3d6a248cc4c67c4ab0aeea47fa7716a85
Darius Yik <darius.yik@gmail.com>
2017-09-17 06:00:08 -0400

    trying to fix create my intierary button

--- public/index.js
+++ public/index.js
@@ -5,8 +5,29 @@ function itinerary() {
   var name = $(".nameInput").val();
   var numDay = $("select[name=Month]").val();
   var length = document.getElementById("dayInput").val();
+/*
+$("#itinerary").val("
+  <th>Your Itinerary for the First Day!</th>
+  <tr>
+    <td>Niagara Falls</td>
+    <td>9:00 AM</td>
+  </tr>
+  <tr>
+    <td>CN Tower</td>
+    <td>10:00 AM</td>
+  </tr>
+  <tr>
+    <td>Royal Ontario Museum</td>
+    <td>12:00 PM</td>
+  </tr>
 
-  for(i=0;i<length;i++) {
-    $(".itinerary").append(numDay);
-  }
+<th>Your Itinerary for the Second Day!</th>
+  <tr>
+    <td>El Catrin</td>
+    <td>11:00 AM</td>
+  </tr>
+  <tr>
+    <td>Downtown</td>
+    <td>1:00 PM</td>
+  </tr>");*/
 }
